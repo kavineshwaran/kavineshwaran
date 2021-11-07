0a54f394d763e5e0b1a5ea1f2c19fb4246b132ba
kavineshwaran <93816975+kavineshwaran@users.noreply.github.com>
2021-11-07 19:04:31 +0530

    Add files via upload

---
+++ ex7.rb
@@ -0,0 +1,21 @@
+puts "kavin had a little bike."
+puts "Its fleece was white as #{'snow'}."
+puts "And everywhere that kavin went drive."
+puts "." * 10  # what'd that do?
+
+end1 = "k"
+end2 = "a"
+end3 = "v"
+end4 = "i"
+end5 = "n"
+end6 = "e"
+end7 = "s"
+end8 = "h"
+end9 = "S"
+end10 = "K"
+end11 = "T"
+end12 = "K"
+
+# watch that print vs. puts on this line what's it do?
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12
